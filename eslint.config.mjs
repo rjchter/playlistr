df46931e8b9bea4cf028da0ded19eb9cbc22009a
rjchter <julius.richter05@gmail.com>
2025-01-05 14:22:34 +0100

    eslint config adjustments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 );
